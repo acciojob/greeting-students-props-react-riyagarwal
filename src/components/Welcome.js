fe40cb4a5cedc95203f3f599b692696bb42d7ab7
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-02-14 10:52:14 +0530

    completed welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+
+const Welcome = ({name}) => {
+    return (
+        <>
+        <h1>Hey {name}!</h1>
+        <h2>Welcome to school</h2>
+        </>
+    )
+}
